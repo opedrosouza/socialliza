337faa315461e2b41e8b4e06093d331afb86b3b9
Pedro Souza <opedrosouza@hotmail.com>
2020-12-24 22:10:23 -0300

    Created user model with devise and started the tests

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email  { Faker::Internet.email }
+    password { '123123' }
+    password_confirmation { '123123' }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,10 +44,13 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.include FactoryBot::Syntax::Methods
 
   config.fixture_path = "#{::Rails.root}/spec/factories"
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
