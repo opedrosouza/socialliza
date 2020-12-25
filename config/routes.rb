d8d3bcb9a41a728c0e2eb3f3350bbbf4e2b31647
Pedro Souza <opedrosouza@hotmail.com>
2020-12-24 23:31:47 -0300

    Finished config for users login and registration with devise and implemented a simple view layout with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }, path: '/', 
+    path_names: { 
+      sign_in: 'entrar',
+      sign_out: 'sair',
+      password: 'secret',
+      confirmation: 'verification',
+      unlock: 'unblock',
+      registration: 'cadastro',
+      sign_up: 'cadastrar'
+    }
+  
   root to: 'pages#home'
 end
